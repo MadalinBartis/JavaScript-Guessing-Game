6336d4f0493121b441037b399eda3eb37b7d973d
MadalinBartis <96352607+MadalinBartis@users.noreply.github.com>
2022-01-18 20:05:05 +0200

    Javascript Guessing Game
    A Javascript Guessing game , where you need to choose a number and then guess the next one

---
+++ app.js
@@ -0,0 +1,31 @@
+//const secret = "BabyHippo";
+//let guess = prompt ("Enter the secret code....");
+//while(guess!==secret){
+  //  guess=prompt("enter the fcking code");
+//}
+//let bla= console.log("Congratulation you got the code")
+
+let maximum=parseInt(prompt("Enter the maximum number"));
+while(!maximum){
+    maximum=parseInt(prompt("Enter a valid number"));
+}
+const targetNum =Math.floor(Math.random()*maximum+1);
+let guess= parseInt (prompt ("enter your first guess"));
+let attempts = 1;
+while(parseInt(guess)!==targetNum){
+  if (guess==='q') break;
+  attempts++;
+  if(guess >targetNum){
+guess=prompt("too high! Enter a new guess:");
+  } else {
+    guess=prompt("too low! enter a new guess");
+
+  }
+
+}
+if (guess==='q'){
+  console.log("Ok, you quit ")
+} else {
+  console.log ("congrats , you win!")
+  console.log(`you got it ! It took you ${attempts} guesses`)
+}
